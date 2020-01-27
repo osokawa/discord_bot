b3de94f3b93a74c8b18fd2c5fa30b8b67d147991
oriaca <oriaca372m@outlook.jp>
2020-01-27 19:18:50 +0900

    mondai にとりあえず繰り返しモードを実装

---
+++ src/features/mondai/game.js
@@ -0,0 +1,201 @@
+const { Attachment } = require('discord.js')
+const { execFile } = require('child_process')
+const os = require('os')
+const fs = require('fs').promises
+const path = require('path')
+const utils = require('../../utils.js')
+
+function generateMondaiImage(mode, inPath, outPath, opts = {}) {
+	const optArgs = []
+	for (const key of Object.keys(opts)) {
+		optArgs.push(`-${key}`)
+		optArgs.push(opts[key])
+	}
+
+	return new Promise((resolve, reject) => {
+		execFile('./tools/mondai.rb', [...optArgs, mode, inPath, outPath], (error, stdout) => {
+			if (error) {
+				reject(error)
+			}
+			try {
+				resolve(JSON.parse(stdout))
+			} catch (e) {
+				reject(e)
+			}
+		})
+	})
+}
+
+function normalizeAnswerMessage(message) {
+	const replaceTables = [
+		[/\s+/g, ' '],
+	]
+	const replaced = replaceTables.reduce((a, i) => a.replace(i[0], i[1]), message)
+	return replaced.normalize('NFKC')
+}
+
+module.exports = class {
+	constructor(channelInstance, mode, options) {
+		this.channelInstance = channelInstance
+		this.feature = channelInstance.feature
+		this.mode = mode
+		this.options = options
+		this.answer = null
+		this.incorrectCount = 0
+		this.correctCount = 0
+		this.incorrectLimit = 3
+	}
+
+	get _isAudioMode() {
+		const audioModes = ['audio', 'music', 'intro']
+		return audioModes.includes(this.mode)
+	}
+
+	get _isMosaicMode() {
+		return this.mode === 'mosaic'
+	}
+
+	async _postMondai() {
+		const episode = utils.randomPick(this.feature.config.episodes)
+		this.tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'mondai-'))
+
+		let outputPath
+
+		if (this._isAudioMode) {
+			outputPath = path.join(this.tmpDir, 'audio.mp3')
+		} else {
+			outputPath = path.join(this.tmpDir, 'image.jpg')
+		}
+
+		const mosaicOriginalPath = path.join(this.tmpDir, 'original.jpg')
+		const options = {}
+		if (this._isMosaicMode) {
+			options.o = mosaicOriginalPath
+		}
+		if (episode.excludeRange) {
+			options.r = episode.excludeRange
+		}
+
+		try {
+			const generateResult = await generateMondaiImage(this.mode, episode.filename, outputPath, options)
+			this.answer = {
+				title: episode.title,
+				pattern: episode.pattern,
+				time: generateResult.time,
+			}
+		} catch (e) {
+			// TODO: 特別なエラー型にラップする
+			throw e
+		}
+
+		const attachment = new Attachment(outputPath)
+		this.channelInstance.channel.send('問題ロボ。頑張るロボ!', attachment)
+	}
+
+	async init() {
+		await this._postMondai()
+	}
+
+	async _postIncorrectLimitMessage(msg, ans, title) {
+		if (this._isMosaicMode) {
+			const attachment = new Attachment(path.join(this.tmpDir, 'original.jpg'))
+			msg.channel.send(`:no_entry_sign: 間違えすぎロボよ… 正解は**${title}**で再生時間は${ans.time}ロボ\nでもモザイクだからしょうがないロボね`
+				+ '\nオリジナルの画像はこれだロボ', attachment)
+		} else {
+			msg.channel.send(`:no_entry_sign: 間違えすぎロボよ…正解は**${title}**で再生時間は${ans.time}ロボ\n出直してくるロボ!`)
+		}
+	}
+
+	async _processAnswerMessage(msg) {
+		const text = normalizeAnswerMessage(msg.content)
+		const ans = this.answer
+		const title = utils.replaceEmoji(ans.title, msg.guild.emojis)
+
+		// 正解
+		if (text.match(new RegExp(ans.pattern, 'i'))) {
+			if (this._isMosaicMode) {
+				const attachment = new Attachment(path.join(this.tmpDir, 'original.jpg'))
+				msg.channel.send(`:ok_hand: 正解ロボ! **${title}** ちなみに再生時間は${ans.time}だロボよ`
+					+ '\nオリジナルの画像はこれロボ。よく頑張ったロボ!', attachment)
+			} else {
+				msg.channel.send(`:ok_hand: 正解ロボ! **${title}** ちなみに再生時間は${ans.time}だロボよ`)
+			}
+
+			if (this.options.repeat) {
+				this.correctCount++
+				await this._postMondai()
+				return true
+			}
+
+			return false
+		}
+
+		// 降参
+		if (text.match(new RegExp(this.feature.config.options.surrenderPattern, 'i'))) {
+			this.incorrectCount++
+			if (this.options.repeat && this.incorrectCount == this.incorrectLimit) {
+				await this._postIncorrectLimitMessage(msg, ans, title)
+				return false
+			}
+
+			if (this._isMosaicMode) {
+				const attachment = new Attachment(path.join(this.tmpDir, 'original.jpg'))
+				msg.channel.send(`情けない子…ロボ! 正解は**${title}**で再生時間は${ans.time}ロボよ\nモザイクは難しいロボね`
+					+ '\nオリジナルの画像はこれだロボ', attachment)
+			} else {
+				msg.channel.send(`情けない子…ロボ! 正解は**${title}**で再生時間は${ans.time}ロボ\n出直すロボ!`)
+			}
+
+			if (this.options.repeat) {
+				await this._postMondai()
+				return true
+			}
+
+			return false
+		}
+
+		// 不正解
+		for (const episode of this.feature.config.episodes) {
+			if (text.match(new RegExp(episode.pattern, 'i'))) {
+				this.incorrectCount++
+				if (this.incorrectCount == this.incorrectLimit) {
+					await this._postIncorrectLimitMessage(msg, ans, title)
+					return false
+				}
+
+				msg.channel.send(':no_entry_sign: 不正解。もうちょっと頑張るか降参するロボ')
+				return true
+			}
+		}
+
+		return true
+	}
+
+	// true なら続行
+	async onMessage(msg) {
+		if (msg.author.bot) {
+			return true
+		}
+
+		const res = await this._processAnswerMessage(msg)
+		if (!res) {
+			this._finalize(msg)
+		}
+		return res
+	}
+
+	async _finalize(msg) {
+		if (this.options.repeat) {
+			let comment = ''
+			if (this.correctCount < 5) {
+				comment = 'もうちょっと頑張るロボ…'
+			} else if (this. correctCount < 10) {
+				comment = 'なかなかやるロボね!'
+			} else {
+				comment = '超スゴイロボ!'
+			}
+			msg.channel.send(`お疲れさまロボ。合計正解数は${this.correctCount}回ロボよ!\n${comment}`)
+		}
+		await fs.rmdir(this.tmpDir, { recursive: true })
+	}
+}
--- src/features/mondai/index.js
+++ src/features/mondai/index.js
@@ -1,185 +1,73 @@
-const { Attachment } = require('discord.js')
-const { execFile } = require('child_process')
-const os = require('os')
 const fs = require('fs').promises
-const path = require('path')
 const utils = require('../../utils.js')
 const TOML = require('@iarna/toml')
-
-function generateMondaiImage(mode, inPath, outPath, opts = {}) {
-	const optArgs = []
-	for (const key of Object.keys(opts)) {
-		optArgs.push(`-${key}`)
-		optArgs.push(opts[key])
-	}
-
-	return new Promise((resolve, reject) => {
-		execFile('./tools/mondai.rb', [...optArgs, mode, inPath, outPath], (error, stdout) => {
-			if (error) {
-				reject(error)
-			}
-			try {
-				resolve(JSON.parse(stdout))
-			} catch (e) {
-				reject(e)
-			}
-		})
-	})
-}
-
-function normalizeAnswerMessage(message) {
-	const replaceTables = [
-		[/\s+/g, ' '],
-	]
-	const replaced = replaceTables.reduce((a, i) => a.replace(i[0], i[1]), message)
-	return replaced.normalize('NFKC')
-}
+const Game = require('./game.js')
 
 class Mondai {
-	constructor(feature) {
+	constructor(feature, channel) {
 		this.feature = feature
-		this._resetState()
+		this.channel = channel
+		this.game = null
 	}
 
-	_resetState() {
-		this.state = 'free'
-		this.answer = null
-		this.incorrectCount = 0
-	}
+	async _processMondaiCommand(rawArgs, msg) {
+		if (this.game !== null) {
+			msg.channel.send('前回の問題がまだ進行中みたいロボ')
+			return
+		}
 
-	async _processMondaiCommand(args, msg) {
-		if (this.state !== 'free') {
-			msg.channel.send('前回の問題にまだ正解していないみたい。わからないなら降参してね')
+		let args, options
+		try {
+			({ args, options } = utils.parseCommandArgs(rawArgs))
+		} catch (e) {
+			msg.channel.send(`変なコマンドを指定していないかロボ?: ${e}`)
 			return
 		}
 
 		const validModes = this.feature.config.options.type === 'music'
 			? ['music', 'intro']
 			: ['image', 'mosaic', 'audio']
-		const audioModes = ['audio', 'music', 'intro']
 
 		let mode = validModes[0]
 		if (1 <= args.length) {
 			if (validModes.includes(args[0])) {
 				mode = args[0]
 			} else {
-				msg.channel.send('知らないモードだなあ…')
+				msg.channel.send('知らないモードロボねぇ…')
 				return
 			}
-		}
-
-		this.state = 'waitingAnswer'
-
-		const episode = utils.randomPick(this.feature.config.episodes)
-
-		const tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'mondai-'))
-		let outputPath
-		if (audioModes.includes(mode)) {
-			outputPath = path.join(tmpDir, 'audio.mp3')
-		} else {
-			outputPath = path.join(tmpDir, 'image.jpg')
-		}
-
-		const mosaicOriginalPath = path.join(tmpDir, 'original.jpg')
-		const options = {}
-		if (mode === 'mosaic') {
-			options.o = mosaicOriginalPath
-		}
-		if (episode.excludeRange) {
-			options.r = episode.excludeRange
 		}
 
 		try {
-			const generateResult = await generateMondaiImage(mode, episode.filename, outputPath, options)
-			this.answer = {
-				title: episode.title,
-				pattern: episode.pattern,
-				time: generateResult.time,
-				mode,
-				tmpDir
-			}
+			this.game = new Game(this, mode, options)
+			await this.game.init(msg)
 		} catch (e) {
-			console.error(e)
-			msg.channel.send('問題の生成中にエラーが発生したロボ…稀によくあるのでめげずにリトライして欲しいロボ')
-			this._resetState()
-			return
-		}
-
-		const attachment = new Attachment(outputPath)
-		msg.channel.send('問題です', attachment)
-	}
-
-	async _processAnswerMessage(msg) {
-		const text = normalizeAnswerMessage(msg.content)
-		const ans = this.answer
-		const title = utils.replaceEmoji(ans.title, msg.guild.emojis)
-		const tmpDir = ans.tmpDir
-
-		// 正解
-		if (text.match(new RegExp(ans.pattern, 'i'))) {
-			if (ans.mode === 'mosaic') {
-				const attachment = new Attachment(path.join(ans.tmpDir, 'original.jpg'))
-				msg.channel.send(`:ok_hand: 正解! **${title}**です! ちなみに再生時間は${ans.time}だよ`
-					+ '\nオリジナルの画像はこちら', attachment)
-			} else {
-				msg.channel.send(`:ok_hand: 正解! **${title}**です! ちなみに再生時間は${ans.time}だよ`)
-			}
-			this._resetState()
-			await fs.rmdir(tmpDir, { recursive: true })
-			return
-		}
-
-		// 降参
-		if (text.match(new RegExp(this.feature.config.options.surrenderPattern, 'i'))) {
-			if (ans.mode === 'mosaic') {
-				const attachment = new Attachment(path.join(ans.tmpDir, 'original.jpg'))
-				msg.channel.send(`情けない子… 正解は**${title}**で再生時間は${ans.time}だよ\nでもモザイクだからしょうがないよね`
-					+ '\nオリジナルの画像はこれだロボ', attachment)
-			} else {
-				msg.channel.send(`情けない子… 正解は**${title}**で再生時間は${ans.time}だよ\n出直しておいで!`)
-			}
-			this._resetState()
-			await fs.rmdir(tmpDir, { recursive: true })
-			return
-		}
-
-		// 不正解
-		for (const episode of this.feature.config.episodes) {
-			if (text.match(new RegExp(episode.pattern, 'i'))) {
-				this.incorrectCount += 1
-
-				if (this.incorrectCount == 3) {
-					if (ans.mode === 'mosaic') {
-						const attachment = new Attachment(path.join(ans.tmpDir, 'original.jpg'))
-						msg.channel.send(`:no_entry_sign: 間違えすぎロボよ… 正解は**${title}**で再生時間は${ans.time}ロボ\nでもモザイクだからしょうがないロボね`
-							+ '\nオリジナルの画像はこれだロボ', attachment)
-					} else {
-						msg.channel.send(`:no_entry_sign: 間違えすぎロボよ…正解は**${title}**で再生時間は${ans.time}ロボ\n出直してくるロボ!`)
-					}
-					this._resetState()
-					await fs.rmdir(tmpDir, { recursive: true })
-					return
-				}
-
-				msg.channel.send(':no_entry_sign: 不正解。もうちょっと頑張るか降参してね')
-				return
-			}
+			this.game = null
+			throw e
 		}
 	}
 
 	async onMessage(msg) {
-		if (msg.author.bot) {
-			return
+		if (!msg.author.bot) {
+			const command = utils.parseCommand(msg.content)
+			if (command && command.commandName === this.feature.cmdname) {
+				await this._processMondaiCommand(command.args, msg)
+				return
+			}
 		}
 
-		const command = utils.parseCommand(msg.content)
-		if (command && command.commandName === this.feature.cmdname) {
-			await this._processMondaiCommand(command.args, msg)
-			return
-		}
+		if (this.game !== null) {
+			let res
+			try {
+				res = await this.game.onMessage(msg)
+			} catch (e) {
+				this.game = null
+				throw e
+			}
 
-		if (this.state === 'waitingAnswer') {
-			await this._processAnswerMessage(msg)
+			if (!res) {
+				this.game = null
+			}
 		}
 	}
 }
