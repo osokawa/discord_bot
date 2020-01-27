bbdd6db0974cfc05b0cba318f43b402b4a279a84
oriaca <oriaca372m@outlook.jp>
2020-01-27 16:09:40 +0900

    utils.js に parseCommandArgs をとりあえず実装

--- src/utils.js
+++ src/utils.js
@@ -12,6 +12,81 @@ exports.parseCommand = function (string) {
 	return { commandName, args }
 }
 
+exports.parseCommandArgs = function (argsToParse, optionsWithValue = [], minimumArgs = 0) {
+	const args = []
+	const options = {}
+
+	for (let i = 0; i < argsToParse.length; i++) {
+		const arg = argsToParse[i]
+
+		if (arg !== '--' && arg.startsWith('--')) {
+			let optName = arg.slice(2)
+			let optValue = true
+
+			const equalIndex = arg.indexOf('=')
+			if (equalIndex !== -1) {
+				optName = arg.slice(2, equalIndex)
+			}
+
+			if (optName === '') {
+				throw `オプション名を指定してください: ${arg}`
+			}
+
+			const isWithValue = optionsWithValue.includes(optName)
+
+			if (isWithValue) {
+				if (equalIndex !== -1) {
+					optValue = arg.slice(equalIndex + 1)
+				} else {
+					if (i + 1 === argsToParse.length) {
+						throw `引数には値が必要です: ${optName}`
+					}
+					i++
+					optValue = argsToParse[i]
+				}
+			} else {
+				if (equalIndex !== -1) {
+					throw `引数は値を持てません: ${optName}`
+				}
+			}
+
+			options[optName] = optValue
+			continue
+		}
+
+		if (arg !== '-' && arg.startsWith('-')) {
+			const opts = arg.slice(1).split('')
+			if (opts.length === 1) {
+				if (optionsWithValue.includes(opts[0])) {
+					if (i + 1 === argsToParse.length) {
+						throw `引数には値が必要です: ${opts[0]}`
+					}
+					i++
+					options[opts[0]] = argsToParse[i]
+					continue
+				}
+			}
+
+			for (const opt of opts) {
+				if (optionsWithValue.includes(opt)) {
+					throw `引数には値が必要です: ${opt}`
+				}
+				options[opt] = true
+			}
+
+			continue
+		}
+
+		args.push(arg)
+	}
+
+	if (args.length < minimumArgs) {
+		throw '引数の数が足りない'
+	}
+
+	return {args, options}
+}
+
 exports.delay = function (ms) {
 	return new Promise(resolve => {
 		setTimeout(() => {
